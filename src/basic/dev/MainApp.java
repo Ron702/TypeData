223abae2bc7bf2af2d9d644d0790e0572edb9df2
Your Name <you@example.com>
2021-10-20 16:30:27 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,48 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+		/**
+		 * khai bao thong tin sinh vien
+		 */
+		System.out.println("Thong tin mot sinh vien:");
+		String ten = "Nguyen Van Ron";
+		System.out.println("Ten Sinh Vien: " + ten);
+		String msv = "2160178";
+		System.out.println("Ma Sinh Vien; " + msv);
+		String cccd = "046203009444";
+		System.out.println("Can Cuoc Cong Dan: " + cccd);
+		String tenLop = "K21CDTH41";
+		System.out.println("Ten Lop: "+ tenLop);
+		String tenTruong = "HueIC";
+		System.out.println("Ten Truong: " + tenTruong);
+		String dtb = "8.5";
+		System.out.println("Diem Trung Binh: " + dtb);
+		System.out.println("<-------------------------------------------->");
+	
+		/**
+		 * khai bao thong tin cong nhan
+		 */
+		String tencn = "phuc";
+		System.out.println("Ten Cong Nhan: " + tencn);
+		String dc = "120nguyenhue";
+		System.out.println("Dia Chi: " + dc);
+		String sodt = "0123456789";
+		System.out.println("So Dien Thoai: " + sodt);
+		String hesl = "800000";
+		System.out.println("He So Luong: " + hesl);
+		System.out.println("<-------------------------------------------->");
+		
+		/**
+		 * khai bao thong tin mot chiec xe car
+		 */
+		String tencx = "tuyen";
+		System.out.println("Ten Chiec Xe: " + tencx);
+		String hang = "lamboghini";
+		System.out.println("Hang: " + hang);
+		String gia = "99999999999";
+		System.out.println("Gia: " + gia);
+		String ms = "xanh";
+		System.out.println("Mau Sac: " + ms);
+	}
+}
